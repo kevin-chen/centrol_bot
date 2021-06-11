c1704a665d6397f4c09e5ad63a25d6cef20c489b
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2021-06-11 13:24:19 -0400

    centering content, bottom right for terms

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ app.config.from_object(config)
 
 @app.route("/")
 def hello_world():
-    return """<!DOCTYPE html>
+    return """!DOCTYPE html>
         <html>
         <head>
             <title>Centrol Trader Bot</title>
         </head>
-        <body style="background-color:black;color:white">
+        <body style="background-color:black;color:white;align-items:center">
             <img src="https://logo.clearbit.com/https://centrol.io"> 
             <h1>Welcome to the Centrol Trader Bot</h1>
             <p>The Trader Bot is a simple tool that connects to your discord server and can serve a whole bunch of awesome financial queries. Check out our Github repo for more information.</p>
-            <div style="text-align:right">
+        </body>
+        <div style="position:absolute;bottom:0;right:0">
             <a href="https://centrol.io/terms.html">Terms</a> | <a href="https://centrol.io/privacy.html">Privacy</a> | © 2021 Centrol.io
             </div>
-        </body>
         </html>"""
 
 
