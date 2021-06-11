355d908089e793acd30ae4548b36b1a1a8e37cab
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2021-06-11 13:08:23 -0400

    testing basic site

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ app.config.from_object(config)
 
 @app.route("/")
 def hello_world():
-    return """<img src="https://logo.clearbit.com/https://centrol.io"></img>If you've made it here, know that you are an exceptional being 🤓. <br>See <a href="https://centrol.io/terms.html">Terms</a> and <a href="https://centrol.io/privacy.html">Privacy</a> @ Centrol.io. <br><br>© 2021 Centrol.io"""
+    return """<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Centrol Trader Bot</title>
+        </head>
+        <body style="background-color:black;color:white">
+            <img src="https://logo.clearbit.com/https://centrol.io"> 
+            <h1>Welcome to the Centrol Trader Bot</h1>
+            <p>The Trader Bot is a simple tool that connects to your discord server and can serve a whole bunch of awesome financial queries. Check out our Github repo for more information.</p>
+            <div style="text-align:right">
+            <a href="https://centrol.io/terms.html">Terms</a> | <a href="https://centrol.io/privacy.html">Privacy</a> | © 2021 Centrol.io
+            </div>
+        </body>
+        </html>"""
 
 
 if __name__ == "__main__":
