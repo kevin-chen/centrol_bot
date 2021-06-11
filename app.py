8f4c1ad3838e60fbc028638c75c49f05a5a9bf16
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2021-06-11 12:46:40 -0400

    testing links and media

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object(config)
 
 @app.route("/")
 def hello_world():
-    return "If you've made it here, know that you are an exceptional being 🤓. <br>See Terms and Privacy @ Centrol.io. <br><br>© 2021 Centrol.io"
+    return "<img src="https://logo.clearbit.com/https://centrol.io"></img>If you've made it here, know that you are an exceptional being 🤓. <br>See <a href="https://centrol.io/terms.html">Terms</a> and <a href="https://centrol.io/privacy.html">Privacy</a> @ Centrol.io. <br><br>© 2021 Centrol.io"
 
 
 if __name__ == "__main__":
