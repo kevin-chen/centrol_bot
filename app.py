03e8988773dac83a0fd4caf05ad0905c9d197079
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2021-06-14 21:45:34 -0400

    testing canva site iframe

--- app.py
+++ app.py
@@ -10,20 +10,7 @@ app.config.from_object(config)
 
 @app.route("/")
 def hello_world():
-    return """<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Centrol Trader Bot</title>
-        </head>
-        <body style="background-color:black;color:white;">
-            <p style="text-align:center"><img src="https://logo.clearbit.com/https://centrol.io"></p>
-            <h1 style="text-align:center">Welcome to the Centrol Trader Bot</h1>
-            <p style="text-align:center">The Trader Bot is a simple tool that connects to your discord server and can serve a whole bunch of awesome financial queries. Check out our Github repo for more information.</p>
-        </body>
-        <div style="position:absolute;bottom:0;right:0;padding-right:5px;padding-bottom:5px">
-            <a href="https://centrol.io/terms.html">Terms</a> | <a href="https://centrol.io/privacy.html">Privacy</a> | © 2021 Centrol.io
-            </div>
-        </html>"""
+    return """<p><iframe src="https://www.canva.com/design/DAEhaTfOe68/D26L3tiyfL1OYxvisRbzmg/view?website#2" width="100%" height="640"></iframe></p>"""
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
