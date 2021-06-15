559c2fc56f55ca1b8964b324a643b14d6eee7fa1
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2021-06-14 21:47:48 -0400

    testing iframe with google https link

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object(config)
 
 @app.route("/")
 def hello_world():
-    return """<p><iframe src="https://www.canva.com/design/DAEhaTfOe68/D26L3tiyfL1OYxvisRbzmg/view?website#2" width="100%" height="640"></iframe></p>"""
+    return """<p><iframe src="https://google.com" width="100%" height="640" border="0"></iframe></p>"""
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
